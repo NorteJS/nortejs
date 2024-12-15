24a127328428edbcacef8a872156af7d27c29902
Fábio Henrique <fabio.h591@gmail.com>
2024-12-15 19:34:28 -0400

    fix(workflows): bun

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: "export"
-  /* config options here */
+  output: 'export',
+  // Outras opções de configuração aqui
 };
 
 export default nextConfig;
