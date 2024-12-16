1f3bae992bc1588ae4b4361687d44b68558900fc
Fábio Henrique <fabio.h591@gmail.com>
2024-12-15 20:06:41 -0400

    fix(workflows): bun

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
    output: 'export',
+   basePath: '/nortejs',
+  assetPrefix: '/nortejs',
   // Outras opções de configuração aqui
 };
 
