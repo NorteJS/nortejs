9b6eb9c9e3a6943cdb6e1e40885589d889bd6b19
fabiobrasileiroo <fabio.h591@gmail.com>
2025-12-15 20:18:39 -0400

    fix: update basePath and assetPrefix in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // basePath: '/',
-  // assetPrefix: '/',
+  basePath: '/nortejs',
+  assetPrefix: '/nortejs',
   images: {
     unoptimized: true,
   },
