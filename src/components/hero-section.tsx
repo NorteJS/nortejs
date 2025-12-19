2711d740392fecab79e5f10cdc65343ca034371c
fabiobrasileiroo <fabio.h591@gmail.com>
2025-12-18 21:13:44 -0400

    feat: padding herosection

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -10,11 +10,11 @@ import { IconJsTs } from "./iconJsTs"
 
 export default function HeroSection() {
   return (
-    <section className="relative bg-blue-950/50 mt-8 max-md:mt-16 max-lg:mt-20 py-12 lg:py-24 min-h-[60vh] flex items-center overflow-hidden">
+    <section className="relative bg-blue-950/50 mt-8 max-md:mt-16 max-lg:mt-20 py-12 lg:py-24 min-h-[70vh] flex items-center overflow-hidden">
       <div className="pointer-events-none absolute inset-x-0 top-0 h-48 bg-gradient-to-b from-black to-transparent" />
-      <div className="pointer-events-none absolute inset-x-0 bottom-0  h-48 bg-gradient-to-t from-black to-transparent" />
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-48 bg-gradient-to-t from-black to-transparent" />
 
-      <div className="relative z-10 container mx-auto px-4 flex flex-col lg:flex-row items-center gap-10">
+      <div className="relativ z-10 container mx-auto px-4 flex flex-col lg:flex-row items-center gap-10">
         <div className="w-full max-w-xl">
           <TypewriterEffectSmoothDemo />
         </div>
